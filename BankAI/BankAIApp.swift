7a8f730188d9eaf28ef786bf0d4ef58aa5e31405
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2025-04-19 16:30:54 +0500

    Initial Commit

---
+++ BankAI/BankAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAIApp.swift
+//  BankAI
+//
+//  Created by Muhammadjon Madaminov on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BankAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
